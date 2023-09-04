17de57a6402a46c666257f1f5fe0545cd94d76db
Yue Li <yueli72@gmail.com>
2023-09-03 21:46:09 -0700

    Add lesson 3

---
+++ lesson_3_spark_glue/customer_landing.sql
@@ -0,0 +1,22 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+  `serialnumber` string COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer',
+  `customername` string COMMENT 'from deserializer',
+  `email` string COMMENT 'from deserializer',
+  `lastupdatedate` bigint COMMENT 'from deserializer',
+  `phone` string COMMENT 'from deserializer',
+  `sharewithfriendsasofdate` float COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedi-lake-house/customer/landing'
+TBLPROPERTIES (
+  'has_encrypted_date'='false', 
+  'transient_lastDdlTime'='1693581966')
